9625eb1d69d8ee85263734684f1990648f222a15
x.chen <x.chen@mpie.de>
2023-07-25 16:19:49 +0200

    Create

---
+++ Euler to Miller/Form1.Designer.cs
@@ -0,0 +1,402 @@
+﻿namespace Euler_to_Miller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBoxH = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBoxK = new System.Windows.Forms.TextBox();
+            this.textBoxI = new System.Windows.Forms.TextBox();
+            this.textBoxU = new System.Windows.Forms.TextBox();
+            this.textBoxV = new System.Windows.Forms.TextBox();
+            this.textBoxT = new System.Windows.Forms.TextBox();
+            this.textBoxL = new System.Windows.Forms.TextBox();
+            this.textBoxW = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(32, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(82, 33);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(26, 21);
+            this.textBox3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(157, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 23);
+            this.button1.TabIndex = 3;
+            this.button1.TabStop = false;
+            this.button1.Text = "E to M";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(157, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(55, 23);
+            this.button2.TabIndex = 3;
+            this.button2.TabStop = false;
+            this.button2.Text = "M to E";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "FCC/BCC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "HCP";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(220, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 23);
+            this.button3.TabIndex = 3;
+            this.button3.TabStop = false;
+            this.button3.Text = "E to M";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(220, 71);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "M to E";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(117, 33);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(32, 21);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBoxH
+            // 
+            this.textBoxH.Location = new System.Drawing.Point(15, 73);
+            this.textBoxH.Name = "textBoxH";
+            this.textBoxH.Size = new System.Drawing.Size(13, 21);
+            this.textBoxH.TabIndex = 5;
+            this.textBoxH.Text = "H";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "[";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(145, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "]";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "(";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(71, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = ")";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(6, 116);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(266, 48);
+            this.textBox7.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 12);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "φ1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(53, 19);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 12);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "φ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(84, 19);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(23, 12);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "φ2";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(122, 19);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(23, 12);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "c/a";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(6, 101);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 12);
+            this.label11.TabIndex = 7;
+            this.label11.Text = "Result:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(4, 58);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(101, 12);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "Miller Indices：";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(4, 5);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(89, 12);
+            this.label13.TabIndex = 7;
+            this.label13.Text = "Euler Angles：";
+            // 
+            // textBoxK
+            // 
+            this.textBoxK.Location = new System.Drawing.Point(29, 73);
+            this.textBoxK.Name = "textBoxK";
+            this.textBoxK.Size = new System.Drawing.Size(13, 21);
+            this.textBoxK.TabIndex = 5;
+            this.textBoxK.Text = "K";
+            // 
+            // textBoxI
+            // 
+            this.textBoxI.Location = new System.Drawing.Point(43, 73);
+            this.textBoxI.Name = "textBoxI";
+            this.textBoxI.Size = new System.Drawing.Size(13, 21);
+            this.textBoxI.TabIndex = 5;
+            this.textBoxI.Text = "I";
+            // 
+            // textBoxU
+            // 
+            this.textBoxU.Location = new System.Drawing.Point(89, 73);
+            this.textBoxU.Name = "textBoxU";
+            this.textBoxU.Size = new System.Drawing.Size(13, 21);
+            this.textBoxU.TabIndex = 5;
+            this.textBoxU.Text = "U";
+            // 
+            // textBoxV
+            // 
+            this.textBoxV.Location = new System.Drawing.Point(103, 73);
+            this.textBoxV.Name = "textBoxV";
+            this.textBoxV.Size = new System.Drawing.Size(13, 21);
+            this.textBoxV.TabIndex = 5;
+            this.textBoxV.Text = "V";
+            // 
+            // textBoxT
+            // 
+            this.textBoxT.Location = new System.Drawing.Point(117, 73);
+            this.textBoxT.Name = "textBoxT";
+            this.textBoxT.Size = new System.Drawing.Size(13, 21);
+            this.textBoxT.TabIndex = 5;
+            this.textBoxT.Text = "T";
+            // 
+            // textBoxL
+            // 
+            this.textBoxL.Location = new System.Drawing.Point(57, 73);
+            this.textBoxL.Name = "textBoxL";
+            this.textBoxL.Size = new System.Drawing.Size(13, 21);
+            this.textBoxL.TabIndex = 5;
+            this.textBoxL.Text = "L";
+            // 
+            // textBoxW
+            // 
+            this.textBoxW.Location = new System.Drawing.Point(131, 73);
+            this.textBoxW.Name = "textBoxW";
+            this.textBoxW.Size = new System.Drawing.Size(13, 21);
+            this.textBoxW.TabIndex = 5;
+            this.textBoxW.Text = "W";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 170);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxW);
+            this.Controls.Add(this.textBoxT);
+            this.Controls.Add(this.textBoxV);
+            this.Controls.Add(this.textBoxU);
+            this.Controls.Add(this.textBoxL);
+            this.Controls.Add(this.textBoxI);
+            this.Controls.Add(this.textBoxK);
+            this.Controls.Add(this.textBoxH);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Euler to Miller";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBoxH;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBoxK;
+        private System.Windows.Forms.TextBox textBoxI;
+        private System.Windows.Forms.TextBox textBoxU;
+        private System.Windows.Forms.TextBox textBoxV;
+        private System.Windows.Forms.TextBox textBoxT;
+        private System.Windows.Forms.TextBox textBoxL;
+        private System.Windows.Forms.TextBox textBoxW;
+    }
+}
+
---
+++ Euler to Miller/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet.Numerics.LinearAlgebra.Double;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+
+
+
+namespace Euler_to_Miller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+
+
+        public double[] primecvt(double[] mart)//可以将输入的小数数组做互质处理后输出
+        {
+            double max1;//用于储存最大的分母
+            double min1;//用于储存最大的分母
+            int a = mart.GetLength(0);//得到输入数组行数
+            string[] b = new string[a];//用于储存转化后的分数
+            double[] sArray = new double[a];//新建二维数组用于储存转化后的分母
+            string[] sArra;
+
+            do
+            {
+                int count = 0;
+                while (count < a)
+                {
+                    b[count] = this.integercvt(mart[count]);//把mart2中各数转成分数存b
+                    sArra = b[count].Split(new char[1] { '/' });//把b中各数按/分割后存到sArra
+                    sArray[count] = Convert.ToDouble(sArra[1]);//sArra取分母转double存sArray
+                    count++;
+                }
+                max1 = sArray.Max();//储存最大的分母
+                for (int i = 0; i < a; i++)
+                {
+                    mart[i] = mart[i] * max1;//mart里面各个数乘上最大分母
+                }
+            }
+            while (max1 != 1);//当最大分母为1时退出
+
+            //#region把Mart里面的数取非0的最小数
+            
+            min1 = 1;//初始值
+            for (int i = 0; i < a; i++)//取出非零的一个绝对值做min1的初始值
+            {
+                if (mart[i] != 0)
+                {
+                    if (mart[i] < 0)
+                    {
+                        min1 = -mart[i];
+                    }
+                    else
+                    {
+                        min1 = mart[i];
+                    }
+                    break;
+                }
+            }
+
+            for (int i = 0; i < a; i++)//取非零最小绝对值min1
+            {
+                
+                if (mart[i] < 0) 
+                {
+                    if (-mart[i] < min1) { min1 = -mart[i]; }
+                }
+                else if (mart[i] == 0)
+                {
+
+                }
+                else 
+                {
+                    if (mart[i] < min1) { min1 = mart[i]; }
+                }
+
+            }
+
+            //#endregion
+        
+            int a2 = a;
+            int n = 0;
+
+            while (n != a2)//当整数数与元素数不同时执行??
+            {
+                n = 0;
+                for (int i = 0; i < a; i++)
+                {
+                    if (mart[i] != 0)
+                    {
+                        if ((Convert.ToDouble((int)(mart[i] / min1))) == mart[i] / min1)//判断是不是整数
+                        {
+                            n++;
+                        }
+                    }
+                    else //如果遇到0就把元素数减1
+                    {
+                        a2--;
+                    }
+
+                }
+                min1--;
+            }
+            min1++;
+
+            for (int i = 0; i < a; i++)
+            {
+                mart[i] = mart[i] / min1;
+            }
+            
+            return mart;
+        }
+        public string integercvt(double numb)//可以将输入的小数转换为分数
+        {
+
+            string result = default(string);
+            try
+            {
+                if (numb > 6E-17)//6e-17看做0
+                {
+                    double inputnum = numb;
+                    string[] array = inputnum.ToString().Split('.');
+                    int len = array[1].Length;
+                    long num = Convert.ToInt64(Math.Pow(10, len));
+                    long value = Convert.ToInt64(inputnum * num);
+                    long a = value;
+                    long b = num;
+                    while (a != b)
+                    {
+                        if (a > b)
+                            a = a - b;
+                        else
+                            b = b - a;
+                    }
+                    value = value / a;
+                    num = num / a;
+                    result = string.Format("{0}/{1}", value, num);
+                }
+                else if (numb < -6E-17)
+                {
+                    double inputnum = numb;
+                    string[] array = inputnum.ToString().Split('-', '.');
+                    int len = array[2].Length;
+                    long num = Convert.ToInt64(Math.Pow(10, len));
+                    long value = Convert.ToInt64(-inputnum * num);
+                    long a = value;
+                    long b = num;
+                    while (a != b)
+                    {
+                        if (a > b)
+                            a = a - b;
+                        else
+                            b = b - a;
+                    }
+                    value = value / a;
+                    num = num / a;
+                    result = string.Format("-{0}/{1}", value, num);
+                }
+                else if (numb < 6E-17 && numb > -6E-17)//6e-17看做0
+                {
+                    result =  "0/1";
+                }
+            }
+            catch (Exception)
+            {
+                result = numb.ToString() + "/1";
+            }
+            return result;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var fai = double.Parse(textBox1.Text);//注意var定义在方法内
+                var fai1 = double.Parse(textBox2.Text);
+                var fai2 = double.Parse(textBox3.Text);
+
+                //创建A，B，C，D矩阵
+                double[] matrixA = new double[3] { Math.Cos(Math.PI * fai1 / 180) * Math.Cos(Math.PI * fai2 / 180) - Math.Sin(Math.PI * fai1 / 180) * Math.Sin(Math.PI * fai2 / 180) * Math.Cos(Math.PI * fai / 180), -Math.Sin(Math.PI * fai2 / 180) * Math.Cos(Math.PI * fai1 / 180) - Math.Sin(Math.PI * fai1 / 180) * Math.Cos(Math.PI * fai2 / 180) * Math.Cos(Math.PI * fai / 180) ,  Math.Sin(Math.PI * fai1 / 180) * Math.Sin(Math.PI * fai / 180)};
+                double[] matrixB = new double[3]{ Math.Sin(Math.PI * fai / 180) * Math.Sin(Math.PI * fai2 / 180) ,  Math.Sin(Math.PI * fai / 180) * Math.Cos(Math.PI * fai2 / 180) ,  Math.Cos(Math.PI * fai / 180) } ;
+
+                //2个矩阵相乘，要注意矩阵乘法的维数要求
+                var m = matrixA;
+                var n= matrixB;
+                m = primecvt(m);
+                n = primecvt(n);
+                textBox7.Text = "(" + m[0].ToString() + "," + m[1].ToString() + "," + m[2].ToString() + ")" + "[" + n[0].ToString() + "," + n[1].ToString() + "," + n[2].ToString() + "]";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Unexpected error");
+            }
+
+
+        }
+        private void button2_Click(object sender, EventArgs e)//fcc m to e
+        {
+            try
+            {
+                double L=System.Convert.ToDouble(textBoxL.Text);
+                double K = System.Convert.ToDouble(textBoxK.Text);
+                double H = System.Convert.ToDouble(textBoxH.Text);
+                double U = System.Convert.ToDouble(textBoxU.Text);
+                double V = System.Convert.ToDouble(textBoxV.Text);
+                double W = System.Convert.ToDouble(textBoxW.Text);
+                double fai=180*Math.Acos(L/(Math.Sqrt((Math.Pow(H,2)+Math.Pow(K,2)+Math.Pow(L,2)))))/Math.PI;
+                double fai1 = 180 * (Math.Asin((W / (Math.Sqrt((Math.Pow(U, 2) + Math.Pow(V, 2) + Math.Pow(W, 2))))) * ((Math.Sqrt((Math.Pow(H, 2) + Math.Pow(K, 2) + Math.Pow(L, 2))) / (Math.Pow(H, 2) + Math.Pow(K, 2)))))) / Math.PI;
+                double fai2 = 180 * Math.Acos(K / (Math.Sqrt((Math.Pow(H, 2) + Math.Pow(K, 2))))) / Math.PI;
+                textBox7.Text = "φ1=" + fai1.ToString() + " φ=" + fai.ToString() + " φ2=" + fai2.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Unexpected error");
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var fai = double.Parse(textBox1.Text);//注意var定义在方法内
+                var fai1 = double.Parse(textBox2.Text);
+                var fai2 = double.Parse(textBox3.Text);
+                var ca = double.Parse(textBox4.Text);
+
+                //创建A，B，C，D矩阵
+                var matrixA = DenseMatrix.OfArray(new[,] { { System.Math.Sqrt(3) / 2, -1 / 2, 0 }, { 0, 1, 0 }, { System.Math.Sqrt(3) / -2, -1 / 2, 0 }, { 0, 0, ca } });
+                var matrixB = DenseMatrix.OfArray(new[,] { { 2 / 3, -1 / 3, 0 }, { 0, 2 / 3, 0 }, { -2 / 3, -1 / 3, 0 }, { 0, 0, ca } });
+                var matrixC = DenseMatrix.OfArray(new[,] { { Math.Cos(Math.PI * fai1 / 180) * Math.Cos(Math.PI * fai2 / 180) - Math.Sin(Math.PI * fai1 / 180) * Math.Sin(Math.PI * fai2 / 180) * Math.Cos(Math.PI * fai / 180) }, { -Math.Sin(Math.PI * fai2 / 180) * Math.Cos(Math.PI * fai1 / 180) - Math.Sin(Math.PI * fai1 / 180) * Math.Cos(Math.PI * fai2 / 180) * Math.Cos(Math.PI * fai / 180) }, { Math.Sin(Math.PI * fai1 / 180) * Math.Sin(Math.PI * fai / 180) } });
+                var matrixD = DenseMatrix.OfArray(new[,] { { Math.Sin(Math.PI * fai / 180) * Math.Sin(Math.PI * fai2 / 180) }, { Math.Sin(Math.PI * fai / 180) * Math.Cos(Math.PI * fai2 / 180) }, { Math.Cos(Math.PI * fai / 180) } });
+
+                //2个矩阵相乘，要注意矩阵乘法的维数要求
+                var resultM = matrixA * matrixC;//也可以使用Multiply方法
+                var resultN = matrixB * matrixD;//也可以使用Multiply方法
+                double[][] resultM2 = resultM.ToRowArrays();//转换为double[][]数组
+                double[] m = new double[resultM2.GetLength(0)];//转换为double[]数组
+                double[][] resultN2 = resultN.ToRowArrays();//转换为double[][]数组
+                double[] n = new double[resultN2.GetLength(0)];//转换为double[]数组
+
+                for (int i = 0; i < resultM2.GetLength(0); i++)
+                {
+                    m[i] = resultM2[i][0];
+                }
+
+                for (int i = 0; i < resultM2.GetLength(0); i++)
+                {
+                    n[i] = resultN2[i][0];
+                }
+                MessageBox.Show("1");
+                m = primecvt(m);
+                n = primecvt(n);
+
+                textBox7.Text = "(" + m[0].ToString() + "," + m[1].ToString() + "," + m[2].ToString() + "," + m[3].ToString() + ")" + "[" + n[0].ToString() + "," + n[1].ToString() + "," + n[2].ToString() + "," + n[3].ToString() + "]";
+
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Unexpected error");
+            }
+
+        }
+    }
+}
